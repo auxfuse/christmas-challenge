32041dbb44b241b33abd8d81c968fa353eb59373
auxfuse <aux.fuse@yahoo.com>
2019-12-12 22:46:16 +0000

    Refactored python code as per collaboration with Shane Muirhead

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ lines = f.readlines()
 
 
 def xmas_tree():
+    """
+    Function to create a Christmas Tree
+    as per Code Institute challenge
+    """
     for line in lines:
         for px in line:
-            print(" " if px == " " else "|" if px == "|" else "/" if px == "/" else "\\" if px == "\\" else "" if px == "\n" else "_", end='')
+            print(" " if px == " " else
+                  "|" if px == "|" else
+                  "/" if px == "/" else
+                  "\\" if px == "\\"
+                  else "" if px == "\n"
+                  else "_", end='')
         print("")
     print("Merry Christmas Code Institute")
 
